6e65828193c929826e9154d2e034cce4bac52a16
First Last <email.mail.com>
2021-05-07 02:38:22 -0500

    first commit

---
+++ src/main/java/com/clouddottech/microservicejar/MicroserviceJarApplication.java
@@ -0,0 +1,20 @@
+package com.clouddottech.microservicejar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MicroserviceJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceJarApplication.class, args);
+	}
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello World from Tomcat!";
+	}
+}
---
+++ src/test/java/com/clouddottech/microservicejar/MicroserviceJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouddottech.microservicejar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
